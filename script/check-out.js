7be7ef6098eb0cec6371e876544e03351e694626
yogesh20031 <khanalyogesh007@gmail.com>
2025-01-16 16:52:11 +0545

    add promise to check-out.js insted of call backs

--- script/check-out.js
+++ script/check-out.js
@@ -4,7 +4,17 @@ import { randerPayment } from "./check-out/right-payment-summary.js";
 // import "../data/backend.js";
 import { loadProducts } from "../data/products.js";
 
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then(() => {
+  randerOrderSummary();
+  randerPayment();
+});
+/*
 loadProducts(() => {
   randerOrderSummary();
   randerPayment();
 });
+*/
